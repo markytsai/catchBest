f0f2ea40c78c27ebbd8c2573480271f79377020e
markytsai <cc129084024@126.com>
2018-07-05 13:31:07 +0800

    是否上传

--- src/main/java/com/ilsxh/catchBest/App.java
+++ src/main/java/com/ilsxh/catchBest/App.java
@@ -9,7 +9,7 @@ import org.springframework.boot.web.support.SpringBootServletInitializer;
  * Hello world!
  *
  */
-// 两个注解有什么区别
+// 两个注解有什么区别啊
 // @EnableAutoConfiguration
 @SpringBootApplication
 public class App  extends SpringBootServletInitializer {
