e6f7600e193988c4510d6bcc9d4485fc586117a9
markytsai <cc129084024@126.com>
2018-07-31 21:02:03 +0800

    加入rabbitmq之前，去掉app.java里面冗余代码

--- src/main/java/com/ilsxh/catchBest/App.java
+++ src/main/java/com/ilsxh/catchBest/App.java
@@ -2,7 +2,6 @@ package com.ilsxh.catchBest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 /**
@@ -12,28 +11,8 @@ import org.springframework.boot.web.support.SpringBootServletInitializer;
 // 两个注解有什么区别啊
 // @EnableAutoConfiguration
 @SpringBootApplication
-public class App  extends SpringBootServletInitializer {
+public class App extends SpringBootServletInitializer {
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(App.class, args);
 	}
-
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-		return builder.sources(App.class);
-	}
-
-	// @Bean
-	// public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
-	// return args -> {
-	//
-	// System.out.println("Let's inspect the beans provided by Spring Boot:");
-	//
-	// String[] beanNames = ctx.getBeanDefinitionNames();
-	// Arrays.sort(beanNames);
-	// for (String beanName : beanNames) {
-	// System.out.println(beanName);
-	// }
-	//
-	// };
-	// }
 }
